7d20472b3883186892399b7cbcbdb0b66b296662
Leonardo Andrés Crespo Sarango <lcrespos@est.ups.edu.ec>
2024-07-09 09:18:51 -0500

    primer commit

--- app.py
+++ app.py
@@ -1,9 +1,197 @@
-from flask import Flask
+import io
+import os
+import sys
+import imageio
+import numpy as np
+import matplotlib.pyplot as plt
+from flask import Flask, jsonify, request, render_template, send_file
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv3D, Conv3DTranspose, Dropout, MaxPooling3D, concatenate
+import keras.backend as K
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
+# Define las funciones de métricas personalizadas
+def dice_coef(y_true, y_pred, smooth=1.0):
+    class_num = 5
+    total_loss = 0
+    for i in range(class_num):
+        y_true_f = K.flatten(y_true[:, :, :, i])
+        y_pred_f = K.flatten(y_pred[:, :, :, i])
+        intersection = K.sum(y_true_f * y_pred_f)
+        loss = ((2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth))
+        total_loss += loss
+    total_loss /= class_num
+    return total_loss
 
-if __name__ == '__main__':
-    app.run()
+
+def dice_coef_necrotic(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:, :, :, 1] * y_pred[:, :, :, 1]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:, :, :, 1])) + K.sum(K.square(y_pred[:, :, :, 1])) + epsilon)
+
+
+def dice_coef_edema(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:, :, :, 2] * y_pred[:, :, :, 2]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:, :, :, 2])) + K.sum(K.square(y_pred[:, :, :, 2])) + epsilon)
+
+
+def dice_coef_enhancing(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:, :, :, 3] * y_pred[:, :, :, 3]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:, :, :, 3])) + K.sum(K.square(y_pred[:, :, :, 3])) + epsilon)
+
+
+def precision(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    return true_positives / (predicted_positives + K.epsilon())
+
+
+def sensitivity(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    return true_positives / (possible_positives + K.epsilon())
+
+
+def specificity(y_true, y_pred):
+    true_negatives = K.sum(K.round(K.clip((1 - y_true) * (1 - y_pred), 0, 1)))
+    possible_negatives = K.sum(K.round(K.clip(1 - y_true, 0, 1)))
+    return true_negatives / (possible_negatives + K.epsilon())
+
+
+# Definir la arquitectura del modelo U-Net
+def UNet(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS, num_classes):
+  inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS))
+  kernel_initializer = 'he_uniform'
+
+  # Downsampling
+  c1 = Conv3D(filters = 16, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(inputs)
+  c1 = Dropout(0.1)(c1)
+  c1 = Conv3D(filters = 16, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(c1)
+  p1 = MaxPooling3D((2, 2, 2))(c1)
+
+  c2 = Conv3D(filters = 32, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(p1)
+  c2 = Dropout(0.1)(c2)
+  c2 = Conv3D(filters = 32, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(c2)
+  p2 = MaxPooling3D((2, 2, 2))(c2)
+
+  c3 = Conv3D(filters = 64, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(p2)
+  c3 = Dropout(0.2)(c3)
+  c3 = Conv3D(filters = 64, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(c3)
+  p3 = MaxPooling3D((2, 2, 2))(c3)
+
+  c4 = Conv3D(filters = 128, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(p3)
+  c4 = Dropout(0.2)(c4)
+  c4 = Conv3D(filters = 128, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(c4)
+  p4 = MaxPooling3D((2, 2, 2))(c4)
+
+  c5 = Conv3D(filters = 256, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(p4)
+  c5 = Dropout(0.3)(c5)
+  c5 = Conv3D(filters = 256, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(c5)
+
+  # Upsampling part
+  u6 = Conv3DTranspose(128, (2, 2, 2), strides=(2, 2, 2), padding='same')(c5)
+  u6 = concatenate([u6, c4])
+  c6 = Conv3D(filters = 128, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(u6)
+  c6 = Dropout(0.2)(c6)
+  c6 = Conv3D(filters = 128, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(c6)
+
+  u7 = Conv3DTranspose(64, (2, 2, 2), strides=(2, 2, 2), padding='same')(c6)
+  u7 = concatenate([u7, c3])
+  c7 = Conv3D(filters = 64, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(u7)
+  c7 = Dropout(0.2)(c7)
+  c7 = Conv3D(filters = 64, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(c7)
+
+  u8 = Conv3DTranspose(32, (2, 2, 2), strides=(2, 2, 2), padding='same')(c7)
+  u8 = concatenate([u8, c2])
+  c8 = Conv3D(filters = 32, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(u8)
+  c8 = Dropout(0.1)(c8)
+  c8 = Conv3D(filters = 32, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(c8)
+
+  u9 = Conv3DTranspose(16, (2, 2, 2), strides=(2, 2, 2), padding='same')(c8)
+  u9 = concatenate([u9, c1])
+  c9 = Conv3D(filters = 16, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(u9)
+  c9 = Dropout(0.1)(c9)
+  c9 = Conv3D(filters = 16, kernel_size = 3, strides = 1, activation='relu', kernel_initializer=kernel_initializer, padding='same')(c9)
+
+  outputs = Conv3D(num_classes, (1, 1, 1), activation='softmax')(c9)
+
+  model = Model(inputs=[inputs], outputs=[outputs])
+
+
+  return model
+
+
+# Crear el modelo y cargar los pesos
+IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS = 128, 128, 160, 4
+num_classes = 5
+
+try:
+    model = UNet(IMG_HEIGHT, IMG_WIDTH, IMG_DEPTH, IMG_CHANNELS, num_classes)
+    model.load_weights('C:/Users/lcres/PycharmProjects/Flask Server Brain Tumor/model_3D/4 clases/modelUnet3D.h5')
+    model_loaded = True
+except Exception as e:
+    model_loaded = False
+    load_error = str(e)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Ruta para manejar la predicción
+@app.route('/predict', methods=['POST'])
+def predict():
+    if not model_loaded:
+        return "Error al cargar el modelo."
+
+    try:
+        # Asegurarse de que se envió un archivo
+        if 'file' not in request.files:
+            return "No se envió ningún archivo."
+
+        file = request.files['file']
+
+        # Cargar la imagen y realizar la predicción
+        test_img = np.load(file)
+        test_img_input = np.expand_dims(test_img, axis=0)
+        test_prediction = model.predict(test_img_input)
+        test_prediction_argmax = np.argmax(test_prediction, axis=4)[0,:,:,:]
+
+        # Crear una lista para almacenar las imágenes del GIF
+        gif_images = []
+
+        # Generar imágenes para cada slice y agregarlas a gif_images
+        for i in range(test_prediction_argmax.shape[2]):
+            fig, ax = plt.subplots(1, 2, figsize=(12, 8))
+
+            ax[0].imshow(test_img[:,:,i,1], cmap='gray')
+            ax[0].title.set_text('Testing Image')
+
+
+            ax[1].imshow(test_prediction_argmax[:,:,i])
+            ax[1].title.set_text('Prediction on test image')
+
+            # Agregar la imagen actual a la lista para el GIF
+            fig.canvas.draw()
+            image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+            image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+            gif_images.append(image)
+
+            # Cerrar la figura para liberar memoria
+            plt.close(fig)
+
+        # Guardar las imágenes como un GIF en memoria
+        gif_bytes = imageio.mimwrite(imageio.RETURN_BYTES, gif_images, format='GIF', fps=20)
+
+        # Enviar el GIF como respuesta
+        return send_file(io.BytesIO(gif_bytes), mimetype='image/gif')
+
+    except Exception as e:
+        # Imprimir el error por consola
+        print("Error durante la predicción:", str(e))
+        return "Error durante la predicción. Consulta los registros del servidor para más detalles."
+
+if __name__ == "__main__":
+    app.run(debug=True)
