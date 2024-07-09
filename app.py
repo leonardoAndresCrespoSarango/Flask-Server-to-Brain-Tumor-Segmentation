95df9a125935ca6d88c74668526b9fd01fab14e0
Leonardo Andrés Crespo Sarango <lcrespos@est.ups.edu.ec>
2024-07-09 10:07:04 -0500

    primer commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import io
+import os
 import sys
 import imageio
 import numpy as np
 import matplotlib.pyplot as plt
-from flask import Flask, jsonify, request, render_template, send_file
+from flask import Flask, jsonify, request, render_template, send_file, redirect, url_for
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv3D, Conv3DTranspose, Dropout, MaxPooling3D, concatenate
 from UNET import dice_coef
@@ -15,6 +16,7 @@ from UNET import sensitivity
 from UNET import specificity
 from UNET import UNet
 from H5 import load_hdf5_file
+from moviepy.editor import ImageSequenceClip
 app = Flask(__name__)
 
 sys.stdin.reconfigure(encoding='utf-8')
@@ -49,9 +51,11 @@ def predict():
             return "No se envió ningún archivo."
 
         file = request.files['file']
+        file_path = "temp_file.h5"
+        file.save(file_path)
 
         # Cargar la imagen y realizar la predicción
-        test_img, _ = load_hdf5_file(file)
+        test_img, _ = load_hdf5_file(file_path)
         if test_img is None:
             return "Error al cargar el archivo HDF5."
 
@@ -59,10 +63,10 @@ def predict():
         test_prediction = model.predict(test_img_input)
         test_prediction_argmax = np.argmax(test_prediction, axis=4)[0, :, :, :]
 
-        # Crear una lista para almacenar las imágenes del GIF
-        gif_images = []
+        # Crear una lista para almacenar las imágenes del video
+        images = []
 
-        # Generar imágenes para cada slice y agregarlas a gif_images
+        # Generar imágenes para cada slice y agregarlas a images
         for i in range(test_prediction_argmax.shape[2]):
             fig, ax = plt.subplots(1, 2, figsize=(12, 8))
 
@@ -72,25 +76,39 @@ def predict():
             ax[1].imshow(test_prediction_argmax[:, :, i])
             ax[1].title.set_text('Prediction on test image')
 
-            # Agregar la imagen actual a la lista para el GIF
+            # Guardar la figura en un buffer de bytes
             fig.canvas.draw()
             image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
             image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-            gif_images.append(image)
 
             # Cerrar la figura para liberar memoria
             plt.close(fig)
 
-        # Guardar las imágenes como un GIF en memoria
-        gif_bytes = imageio.mimwrite(imageio.RETURN_BYTES, gif_images, format='GIF', fps=20)
+            # Agregar la imagen al arreglo de imágenes
+            images.append(image)
 
-        # Enviar el GIF como respuesta
-        return send_file(io.BytesIO(gif_bytes), mimetype='image/gif')
+        # Crear un clip de video con las imágenes
+        video_filename = "static/temp_video.mp4"
+        clip = ImageSequenceClip(images, fps=1)
+        clip.write_videofile(video_filename, codec='libx264', audio=False)
+
+        # Eliminar archivos temporales
+        os.remove(file_path)
+
+        # Redirigir a la página de resultados
+        return redirect(url_for('result', video_filename="temp_video.mp4"))
 
     except Exception as e:
         # Imprimir el error por consola
         print("Error durante la predicción:", str(e))
         return "Error durante la predicción. Consulta los registros del servidor para más detalles."
+
+@app.route('/result')
+def result():
+    video_filename = request.args.get('video_filename')
+    video_url = url_for('static', filename=video_filename)
+    print(f"Video URL: {video_url}")
+    return render_template('result.html', video_url=video_url)
 
 if __name__ == "__main__":
     app.run(debug=True)
